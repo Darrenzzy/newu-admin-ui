0add9cab519a046e0250abec4b67bc9c3025f331
darren <zzyphp@gmail.com>
2021-01-11 13:26:49 +0800

    [style] 删除 css

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+  cd ../service/view
+  module_name='admin'
+  tar cvzf "$module_name".tar.gz admin
+
+  scp "$module_name".tar.gz root@119.28.10.43:/www/wwwroot/admin.newuinvest.com
+
+  ssh root@119.28.10.43 /www/wwwroot/admin.newuinvest.com/restart.sh
+
